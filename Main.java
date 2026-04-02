15c6f77347baea14668c38fb77249db16fc880c8
ZakiullahAfzal <101314613+ZakiullahAfzal@users.noreply.github.com>
2026-04-02 12:45:20 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,26 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a text: ");
-        StringBuilder value = new StringBuilder(input.nextLine());
-        StringBuilder getValue = new StringBuilder();
-        for (int index : value.toString().toCharArray()) {
-            getValue.append(Encryption.divider(String.valueOf(index))).append("_");
-        }
-        System.out.println(getValue);
-        StringBuilder setValue = new StringBuilder();
-         value = new StringBuilder();
-        for (char index : getValue.toString().toCharArray()) {
-            if (index == '_') {
-                setValue.append((char)Integer.parseInt(Encryption.login(value.toString(), true)));
-                value = new StringBuilder();
-            } else {
-                value.append(index);
-            }
-        }
+        String encrypted1 = Encryption.encrypt("Hello", 1);
+        String encrypted2 = Encryption.encrypt("Hello", 2);
+        String encrypted3 = Encryption.encrypt("Hello", 3);
 
-        System.out.println(setValue);
+        System.out.println(encrypted1);
+        System.out.println(encrypted2);
+        System.out.println(encrypted3);
 
+        System.out.println(Encryption.decrypt(encrypted1, 1));
+        System.out.println(Encryption.decrypt(encrypted2, 2));
+        System.out.println(Encryption.decrypt(encrypted3, 3));
     }
 }
